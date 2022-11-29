7d7e835fb1ca1f5578cbac9c64e59ff04bd83a66
Elvis Rodríguez Durán <64595162+ElvisRD@users.noreply.github.com>
2022-11-29 20:34:37 -0300

    Update 4.sql

--- etapa-1/4.sql
+++ etapa-1/4.sql
@@ -1,4 +1,19 @@
 --4. (30 ptos.) Crear un trigger que no permita registrar informaci´on de empleadores mientras
 --el estudiante no tenga el estado de graduado.
 
-CREATE OR
+CREATE OR REPLACE TRIGGER NO_EMPLEADORES BEFORE INSERT ON empleador
+FOR EACH ROW
+DECLARE 
+estado_estudiante estado.esta_descripcion%type;
+
+BEGIN 
+  SELECT E.esta_descripcion INTO estado_estudiante FROM estudiante E, tiene_estado TE, estado E, tiene_emp TEM, empleador EM
+  WHERE E.est_rut=TEM.est_rut AND TEM.emp_rut=EM.emp_rut
+  AND E.est_rut=TE.est_rut AND TE.esta_id=E.esta_id;
+  
+  IF estado_estudiante = 'Graduado' THEN 
+    RAISE_APPLICATION_ERROR(-20300,'El estudiante no se encuentra graduado.');  
+  END IF;
+ END;
+
+
