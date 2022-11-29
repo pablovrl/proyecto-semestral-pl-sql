c8ff28353abde71ef40dcef0dd4066a40829a4a8
Elvis Rodríguez Durán <64595162+ElvisRD@users.noreply.github.com>
2022-11-29 17:46:56 -0300

    Create 3.sql

---
+++ etapa-1/3.sql
@@ -0,0 +1,18 @@
+--3. (30 ptos.) Crear un trigger que debe validar que un estudiante que tenga una nota final
+--inferior a 4,0 su estado debe ser reprobado, de lo contrario, debe ser aprobado. En caso
+--que esto ocurra, se debe cambiar el estado de acuerdo a la nota registrada.
+
+CREATE OR REPLACE TRIGGER ESTA_APROBADO AFTER INSERT ON inscribe FOR EACH ROW
+DECLARE
+notaFinal FLOAT;
+
+
+BEGIN 
+  SELECT I.nota into notaFinal from estudiante E, inscribe I
+  WHERE E.est_rut=I.est_rut  
+  IF 4,0 < notaFinal THEN
+    UPDATE inscribe SET estado = 'Reprobado'
+  ELSE 
+    UPDATE inscribe SET estado = 'Aprobado'
+  END IF;
+END;
