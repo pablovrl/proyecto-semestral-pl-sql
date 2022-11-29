8dec1cbfaa921324f5e69e8cc8b4a7ea4418d90c
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-11-29 16:24:26 -0300

    update 1.sql

--- etapa-1/1.sql
+++ etapa-1/1.sql
@@ -2,18 +2,18 @@
 -- estudiantes graduado que ha tenido cada programa durante el tiempo.
 
 DECLARE 
-CURSOR C is 
+  CURSOR C IS 
   SELECT P.pos_nombre, COUNT (*) FROM programa P, matricula M, estudiante E, tiene_estado TE, estado ES
-  where P.pos_codigo = M.pos_codigo AND M.est_rut= E.est_rut AND E.est_rut = TE.est_rut
+  WHERE P.pos_codigo = M.pos_codigo AND M.est_rut= E.est_rut AND E.est_rut = TE.est_rut
   AND TE.esta_id = ES.esta_id AND ES.esta_descripcion = 'Graduado' GROUP BY P.pos_nombre;
-  programa programa.pos_nombre%type;
-  cantidad integer;
+  programa programa.pos_nombre%TYPE;
+  cantidad INTEGER;
 BEGIN 
   OPEN C;
-  FETCH C into programa, cantidad
-  WHILE C%found loop
+  FETCH C INTO programa, cantidad;
+  WHILE C%FOUND LOOP
     dbms_output.put_line('El programa ' || programa || ' tiene ' || cantidad || ' estudiantes graduados.');
-    FETCH C into programa, cantidad;
-  end loop;
+    FETCH C INTO programa, cantidad;
+  END LOOP;
   CLOSE C;
 END;
