fbcda07a28d8ce85dec811c75fc42997736c2e8e
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-11-30 15:51:08 -0300

    update 2.sql

--- etapa-1/2.sql
+++ etapa-1/2.sql
@@ -9,6 +9,17 @@ DECLARE
   FROM Estado ES, tiene_estado TE, Estudiante E, Matricula M, Programa P
   WHERE ES.esta_id = TE.esta_id AND TE.est_rut = E.est_rut AND E.est_rut = M.est_rut AND M.pos_codigo = P.pos_codigo
   GROUP BY ES.esta_descripcion, P.pos_nombre;
+  estado varchar(100);
+  programa varchar(100);
+  cantidad integer;
+
+  CURSOR C2 IS
+  SELECT ASO.rol, count(*)
+  FROM Academico A, ASOCIADO ASO
+  WHERE A.aca_rut = ASO.aca_rut
+  group by ASO.rol;
+  rol varchar(100);
+  cant_rol integer;
 BEGIN
   OPEN C;
   FETCH C INTO estado, cantidad, programa;
@@ -17,4 +28,12 @@ BEGIN
     FETCH C INTO estado, cantidad, programa;
   END LOOP;
   CLOSE C;
+  
+  OPEN C2;
+  FETCH C2 INTO rol, cant_rol;
+  WHILE C2%FOUND LOOP
+    dbms_output.put_line('Existen ' || cant_rol || ' académico/s con el rol: ' || rol);
+    FETCH C2 INTO rol, cant_rol;
+  END LOOP;
+  CLOSE C2;
 END;
