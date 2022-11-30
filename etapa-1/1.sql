1861b94eadba7c5e44ea52928db5ce8f8aa6b34f
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-11-30 01:15:25 -0300

    update 1.sql

--- etapa-1/1.sql
+++ etapa-1/1.sql
@@ -3,17 +3,18 @@
 
 DECLARE 
   CURSOR C IS 
-  SELECT P.pos_nombre, COUNT (*) FROM programa P, matricula M, estudiante E, tiene_estado TE, estado ES
-  WHERE P.pos_codigo = M.pos_codigo AND M.est_rut= E.est_rut AND E.est_rut = TE.est_rut
-  AND TE.esta_id = ES.esta_id AND ES.esta_descripcion = 'Graduado' GROUP BY P.pos_nombre;
-  programa programa.pos_nombre%TYPE;
+  SELECT p.pos_nombre, count(*) FROM programa P, matricula M, estudiante E, tiene_estado TE, estado ES
+  WHERE P.pos_codigo = M.pos_codigo AND M.est_rut= E.est_rut AND E.est_rut=TE.est_rut
+  AND TE.esta_id=ES.esta_id AND ES.esta_descripcion='Graduado' GROUP BY p.pos_nombre;
+  
+  pr programa.pos_nombre%TYPE;
   cantidad INTEGER;
 BEGIN 
   OPEN C;
-  FETCH C INTO programa, cantidad;
+  FETCH C INTO pr, cantidad;
   WHILE C%FOUND LOOP
-    dbms_output.put_line('El programa ' || programa || ' tiene ' || cantidad || ' estudiantes graduados.');
-    FETCH C INTO programa, cantidad;
+    dbms_output.put_line('El programa ' || pr || ' tiene ' || cantidad || ' estudiantes graduados.');
+    FETCH C INTO pr, cantidad;
   END LOOP;
   CLOSE C;
 END;
