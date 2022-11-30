6d852d4d28af4fa6f12455e9bd63ab8de55ff7aa
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-11-30 01:14:13 -0300

    update 2.sql

--- etapa-1/2.sql
+++ etapa-1/2.sql
@@ -1,15 +1,20 @@
---2. (20 ptos.) Genere un programa en PL/SQL que sea capaz de mostrar el nombre de los
---programas y la cantidad de estudiantes que se encuentran en los distintos estados. Adem´as,
---de mostrar la cantidad de acad´emicos que son claustro y la cantidad de acad´emicos que
---son colaboradores durante el semestre actual.
+-- 2. (20 ptos.) Genere un programa en PL/SQL que sea capaz de mostrar el nombre de los
+-- programas y la cantidad de estudiantes que se encuentran en los distintos estados. Adem´as,
+-- de mostrar la cantidad de acad´emicos que son claustro y la cantidad de acad´emicos que
+-- son colaboradores durante el semestre actual.
 
 DECLARE 
   CURSOR C IS 
-  SELECT *, COUNT(*) from programa P, matricula M, estudiante E, tiene_estado TE, estado E
-  WHERE  P.pos_codigo=M.pos_codigo AND M.est_rut=E.est_rut AND E.est_rut=TE.est_rut 
-  AND TE.esta_id=E.esta_id GROUP BY 
-  CURSOR C2
-  
+  SELECT ES.esta_descripcion, COUNT(*), P.pos_nombre
+  FROM Estado ES, tiene_estado TE, Estudiante E, Matricula M, Programa P
+  WHERE ES.esta_id = TE.esta_id AND TE.est_rut = E.est_rut AND E.est_rut = M.est_rut AND M.pos_codigo = P.pos_codigo
+  GROUP BY ES.esta_descripcion, P.pos_nombre;
 BEGIN
-  
-  
+  OPEN C;
+  FETCH C INTO estado, cantidad, programa;
+  WHILE C%FOUND LOOP
+    dbms_output.put_line('El programa ' || programa || ' actualmente tiene ' || cantidad || ' estudiante/s con estado ' || estado);
+    FETCH C INTO estado, cantidad, programa;
+  END LOOP;
+  CLOSE C;
+END;
