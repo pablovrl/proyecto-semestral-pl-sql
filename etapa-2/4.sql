efc42a943e3be0f07d024916ab098e1aa7b15468
Pablo Villarroel <pablovillarroel135@gmail.com>
2023-01-01 15:23:45 -0300

    create 4.sql

---
+++ etapa-2/4.sql
@@ -0,0 +1,3 @@
+-- (20 ptos.) De acuerdo a todo lo planteado en este enunciado (parte I y II), defina dos
+-- ındices que permita mejorar el rendimiento de esta Base de Datos. 
+--No se debe considerar los  ́ındices impĺıcitos de las claves primarias.
