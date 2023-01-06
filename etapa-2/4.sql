9504ba800896c3e10627dc8338a92ac18ae39239
Pablo Villarroel <pablovillarroel135@gmail.com>
2023-01-06 00:41:02 -0300

    finish etapa-2 4.sql

--- etapa-2/4.sql
+++ etapa-2/4.sql
@@ -1,3 +1,9 @@
 -- (20 ptos.) De acuerdo a todo lo planteado en este enunciado (parte I y II), defina dos
 -- ındices que permita mejorar el rendimiento de esta Base de Datos. 
---No se debe considerar los  ́ındices impĺıcitos de las claves primarias.
+-- No se debe considerar los  ́ındices impĺıcitos de las claves primarias.
+
+CREATE BITMAP INDEX IN_ESTADO 
+ON ESTADO(ESTA_DESCRIPCION);
+
+CREATE BITMAP INDEX IN_PROGRAMA 
+ON PROGRAMA(POS_NOMBRE);
